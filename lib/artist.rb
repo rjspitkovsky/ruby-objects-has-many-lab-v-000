ae8901713ad525969928c86daf6b3f6a2acad3a4
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-01 22:49:21 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,8 +15,8 @@ attr_accessor :name
     song.artist = self
   end
 
-  def add_song_by_name(name, genre)
-    song = Song.new(name, genre)
+  def add_song_by_name(name)
+    song = Song.new(name)
     @songs << song
     song.artist = self
   end
